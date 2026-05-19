3fcd169c56595b687474fdba4ff752e9f72a2f09
Developerr-here <shanzayy.shafiq@gmail.com>
2026-05-19 16:18:55 +0500

    axios url changed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,21 +1,21 @@
-import axios from 'axios';
-
-// Check if a production URL exists, otherwise fallback to local development
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
-
-const api = axios.create({
-  baseURL: API_URL,
-  withCredentials: true // Crucial for keeping your users logged in via JWT cookies
-});
-
-export default api;
-
-
 // import axios from 'axios';
 
+// // Check if a production URL exists, otherwise fallback to local development
+// const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
 // const api = axios.create({
-//   baseURL: 'http://localhost:5000/api',
-//   withCredentials: true, // Crucial for sending/receiving cookies
+//   baseURL: API_URL,
+//   withCredentials: true // Crucial for keeping your users logged in via JWT cookies
 // });
 
 // export default api;
+
+
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  withCredentials: true, // Crucial for sending/receiving cookies
+});
+
+export default api;
